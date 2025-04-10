431e739ab84c899c7193e2bf770646b0e54780cd
Andy <andytony56791@gmail.com>
2025-04-09 19:09:48 -0700

    Updates

---
+++ precache-manifest.2aa60c6183f68e839ecaffd7f43e3622.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8df099e4cc4dc682763741bd90cbb31e",
+    "url": "/Andy-Personal-Website/index.html"
+  },
+  {
+    "revision": "f19a7c9306ee5f136dd2",
+    "url": "/Andy-Personal-Website/static/css/main.e96f071d.chunk.css"
+  },
+  {
+    "revision": "83ff7e6725e5aa11d642",
+    "url": "/Andy-Personal-Website/static/js/2.e54f28c3.chunk.js"
+  },
+  {
+    "revision": "2984d7dc8ce4c59bfce177dbd6f36e9c",
+    "url": "/Andy-Personal-Website/static/js/2.e54f28c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f19a7c9306ee5f136dd2",
+    "url": "/Andy-Personal-Website/static/js/main.3dd1ade8.chunk.js"
+  },
+  {
+    "revision": "1889d4ecd69ed6236279",
+    "url": "/Andy-Personal-Website/static/js/runtime-main.70d9a9b4.js"
+  },
+  {
+    "revision": "b05aabc503de00d78dcd3a4d7bf0593b",
+    "url": "/Andy-Personal-Website/static/media/Andy_CV_042025.b05aabc5.pdf"
+  },
+  {
+    "revision": "e12792a9e6940ec4d6a955f5e29539d0",
+    "url": "/Andy-Personal-Website/static/media/CRUW3D_cover.e12792a9.jpg"
+  },
+  {
+    "revision": "d020252fa6292bdc9f852f467c81c822",
+    "url": "/Andy-Personal-Website/static/media/CrossCheck_Cover.d020252f.jpg"
+  },
+  {
+    "revision": "54486c24b8cc8e811558b13d1a735746",
+    "url": "/Andy-Personal-Website/static/media/MonoTAKD_cover.54486c24.jpg"
+  },
+  {
+    "revision": "96451585f6ae2efce209e12121e0a593",
+    "url": "/Andy-Personal-Website/static/media/andy.96451585.jpg"
+  },
+  {
+    "revision": "7be25869beb7453793c3656ff858a21b",
+    "url": "/Andy-Personal-Website/static/media/cenrad_banner.7be25869.jpg"
+  },
+  {
+    "revision": "5ffd8f46102784517022fc5ed3298fa2",
+    "url": "/Andy-Personal-Website/static/media/game-illusionization_cover.5ffd8f46.jpg"
+  },
+  {
+    "revision": "3f8f42f7fe6d483c15157601201b8da2",
+    "url": "/Andy-Personal-Website/static/media/hapticGoRoundImg1.3f8f42f7.jpg"
+  },
+  {
+    "revision": "d25ed4d4d11e2d60dbe2178554fd58b1",
+    "url": "/Andy-Personal-Website/static/media/hapticGoRoundImg2.d25ed4d4.jpg"
+  },
+  {
+    "revision": "d0320b04871274e8d1876940a6f0cebb",
+    "url": "/Andy-Personal-Website/static/media/hapticGoRoundImg3.d0320b04.jpg"
+  },
+  {
+    "revision": "32bdad959b54321f516da9f7886979fd",
+    "url": "/Andy-Personal-Website/static/media/hapticGoRoundImg4.32bdad95.jpg"
+  },
+  {
+    "revision": "890abf00fb164a68e61588cb5b376668",
+    "url": "/Andy-Personal-Website/static/media/hapticGoRoundImg5.890abf00.jpg"
+  },
+  {
+    "revision": "d844a137f74a4c1999e57490c3d84218",
+    "url": "/Andy-Personal-Website/static/media/impossible-staircase_banner.d844a137.jpg"
+  },
+  {
+    "revision": "c708cea23fa58d62aeb29ad2a71cb5cf",
+    "url": "/Andy-Personal-Website/static/media/impossible-staircase_cover.c708cea2.jpg"
+  },
+  {
+    "revision": "8af21047abe47578680265d6611535cb",
+    "url": "/Andy-Personal-Website/static/media/rtpose_cover.8af21047.jpg"
+  }
+]);
