103718e7d6222569ea18185743d5a6cbda6b1baa
Andy <andytony56791@gmail.com>
2025-04-10 09:08:27 -0700

    Updates

---
+++ precache-manifest.b7061bfe4fa8df5c63c77029fd614bbc.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f88704a49054fc2582014c58c11fa296",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79b63eab3a6bc9b0b8be",
+    "url": "/static/css/main.e96f071d.chunk.css"
+  },
+  {
+    "revision": "b5e1f7107c68eebf3ef8",
+    "url": "/static/js/2.13ee4a5c.chunk.js"
+  },
+  {
+    "revision": "2984d7dc8ce4c59bfce177dbd6f36e9c",
+    "url": "/static/js/2.13ee4a5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79b63eab3a6bc9b0b8be",
+    "url": "/static/js/main.ca57f0b5.chunk.js"
+  },
+  {
+    "revision": "dc52b4b5f3346a93a0b3",
+    "url": "/static/js/runtime-main.3ffe4768.js"
+  },
+  {
+    "revision": "b05aabc503de00d78dcd3a4d7bf0593b",
+    "url": "/static/media/Andy_CV_042025.b05aabc5.pdf"
+  },
+  {
+    "revision": "e12792a9e6940ec4d6a955f5e29539d0",
+    "url": "/static/media/CRUW3D_cover.e12792a9.jpg"
+  },
+  {
+    "revision": "d020252fa6292bdc9f852f467c81c822",
+    "url": "/static/media/CrossCheck_Cover.d020252f.jpg"
+  },
+  {
+    "revision": "54486c24b8cc8e811558b13d1a735746",
+    "url": "/static/media/MonoTAKD_cover.54486c24.jpg"
+  },
+  {
+    "revision": "96451585f6ae2efce209e12121e0a593",
+    "url": "/static/media/andy.96451585.jpg"
+  },
+  {
+    "revision": "7be25869beb7453793c3656ff858a21b",
+    "url": "/static/media/cenrad_banner.7be25869.jpg"
+  },
+  {
+    "revision": "5ffd8f46102784517022fc5ed3298fa2",
+    "url": "/static/media/game-illusionization_cover.5ffd8f46.jpg"
+  },
+  {
+    "revision": "3f8f42f7fe6d483c15157601201b8da2",
+    "url": "/static/media/hapticGoRoundImg1.3f8f42f7.jpg"
+  },
+  {
+    "revision": "d25ed4d4d11e2d60dbe2178554fd58b1",
+    "url": "/static/media/hapticGoRoundImg2.d25ed4d4.jpg"
+  },
+  {
+    "revision": "d0320b04871274e8d1876940a6f0cebb",
+    "url": "/static/media/hapticGoRoundImg3.d0320b04.jpg"
+  },
+  {
+    "revision": "32bdad959b54321f516da9f7886979fd",
+    "url": "/static/media/hapticGoRoundImg4.32bdad95.jpg"
+  },
+  {
+    "revision": "890abf00fb164a68e61588cb5b376668",
+    "url": "/static/media/hapticGoRoundImg5.890abf00.jpg"
+  },
+  {
+    "revision": "d844a137f74a4c1999e57490c3d84218",
+    "url": "/static/media/impossible-staircase_banner.d844a137.jpg"
+  },
+  {
+    "revision": "c708cea23fa58d62aeb29ad2a71cb5cf",
+    "url": "/static/media/impossible-staircase_cover.c708cea2.jpg"
+  },
+  {
+    "revision": "8af21047abe47578680265d6611535cb",
+    "url": "/static/media/rtpose_cover.8af21047.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Andy-Personal-Website/precache-manifest.2aa60c6183f68e839ecaffd7f43e3622.js"
+  "/precache-manifest.b7061bfe4fa8df5c63c77029fd614bbc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Andy-Personal-Website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
